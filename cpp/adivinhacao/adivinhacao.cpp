de36cc00dcac0996f41f12037844313fd434d944
Marcio Coelho Socorro <marcioc@gmail.com>
2023-10-21 19:29:10 -0300

    Ajuste estrutura pastas

---
+++ cpp/adivinhacao/adivinhacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*************************************" << endl;
+    cout << "* Bem vindo ao jogo de adivinhacao! *" << endl;
+    cout << "*************************************" << endl;
+
+    int numeroSecreto = 31;
+    cout << "O numero secreto eh " << numeroSecreto << ", nao conte para ninguem!" << endl;
+}
