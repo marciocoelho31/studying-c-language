fd5cc202d9120a081def75440792c701dd628d9a
Marcio Coelho Socorro <marcioc@gmail.com>
2023-11-20 12:49:43 -0300

    Ignorando arquivos *.o e ajustando cabeçalho

--- cpp/jogodaforca/imprimeCabecalho.cpp
+++ cpp/jogodaforca/imprimeCabecalho.cpp
@@ -3,7 +3,7 @@
 void imprimeCabecalho()
 {
     std::cout << "*********************" << std::endl;
-    std::cout << "*** Jogo da Forca ***" << std::endl;
+    std::cout << "*** JOGO DA FORCA ***" << std::endl;
     std::cout << "*********************" << std::endl;
     std::cout << std::endl;
 }
